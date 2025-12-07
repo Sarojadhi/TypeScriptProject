af3afc3a590436894f9b256b1a1f495719e9a07d
Saroj Adhikari <sarojadh1111@gmail.com>
2025-12-07 20:12:49 +0545

    pla withFunction-Alias

---
+++ Overiew/DataType/function.js
@@ -0,0 +1,23 @@
+/* function product (){
+  var items=50;
+var qt = 5;
+console.log(items*qt)
+}
+product() */
+export {};
+/* const product = (item:number,product:number, text:string)=>{
+
+
+console.log(item*product+" " + text);
+}
+product(5,10,"this is the Total Price") */
+//Type Alias
+/* type User = {
+  name: string;
+  age: number;
+};
+
+const person: User = { name: "Saroj", age: 20 };
+console.log(person)
+ */
+//# sourceMappingURL=function.js.map
